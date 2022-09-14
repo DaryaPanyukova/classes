1d17ba78b7bee9b18a3d77359ba520848d4a9c2c
DaryaPanyukova <panyukova.darya@bk.ru>
2022-09-14 10:46:30 +0300

    first commit

---
+++ class Fraction/Класс Дробей.cpp
@@ -0,0 +1,346 @@
+﻿#pragma once
+#include <exception>
+#include <iostream>
+#include <algorithm>
+
+int gcd(int first_number, int second_number) {
+    first_number = abs(first_number);
+    second_number = abs(second_number);
+    while (second_number) {
+        first_number %= second_number;
+        std::swap(first_number, second_number);
+    }
+    return first_number;
+}
+
+int lcd(int first_number, int second_number) {
+    return (first_number * second_number) / gcd(first_number, second_number);
+}
+
+class Fraction {
+ public:
+  Fraction();
+
+  Fraction(int num, int den);
+  explicit Fraction(int num);
+
+  Fraction(const Fraction& other);
+
+  ~Fraction() = default;
+
+  int num() const { return num_; }
+  int den() const { return den_; }
+
+  /* your code here */
+  friend std::ostream& operator<<(std::ostream& out, const Fraction& fract);
+
+  friend Fraction operator+(Fraction lhs, Fraction rhs);
+  friend Fraction operator+(Fraction lhs, int rhs);
+  friend Fraction operator+(int lhs, Fraction rhs);
+
+  friend Fraction operator-(Fraction lhs, Fraction rhs);
+  friend Fraction operator-(Fraction lhs, int rhs);
+  friend Fraction operator-(int lhs, Fraction rhs);
+
+  friend Fraction operator*(Fraction lhs, Fraction rhs);
+  friend Fraction operator*(Fraction lhs, int rhs);
+  friend Fraction operator*(int lhs, Fraction rhs);
+
+  friend Fraction operator/(Fraction lhs, Fraction rhs);
+  friend Fraction operator/(Fraction lhs, int rhs);
+  friend Fraction operator/(int lhs, Fraction rhs);
+
+  Fraction& operator=(Fraction& other);
+
+  Fraction operator+() { return *this; }
+  Fraction operator-() {
+    num_ = -num_;
+    return *this;
+  }
+
+  Fraction& operator++() {
+    *this += 1;
+    return *this;
+  }
+  Fraction& operator--() {
+    *this -= 1;
+    return *this;
+  }
+  Fraction operator++(int notused) {
+    Fraction val = *this;
+    ++(*this);
+    return val;
+  }
+  Fraction operator--(int notused) {
+    Fraction val = *this;
+    --(*this);
+    return val;
+  }
+
+  Fraction& operator+=(Fraction other);
+  Fraction& operator+=(int other);
+
+  Fraction& operator-=(Fraction other);
+  Fraction& operator-=(int other);
+
+  Fraction& operator*=(Fraction other);
+  Fraction& operator*=(int other);
+
+  Fraction& operator/=(Fraction other);
+  Fraction& operator/=(int other);
+
+  friend bool operator<(Fraction lhs, Fraction rhs);
+  friend bool operator<(Fraction lhs, int rhs);
+  friend bool operator<(int lhs, Fraction rhs);
+
+  friend bool operator>(Fraction lhs, Fraction rhs);
+  friend bool operator>(Fraction lhs, int rhs);
+  friend bool operator>(int lhs, Fraction rhs);
+
+  friend bool operator<=(int lhs, Fraction rhs);
+  friend bool operator<=(Fraction lhs, Fraction rhs);
+  friend bool operator<=(Fraction lhs, int rhs);
+
+  friend bool operator>=(int lhs, Fraction rhs);
+  friend bool operator>=(Fraction lhs, Fraction rhs);
+  friend bool operator>=(Fraction lhs, int rhs);
+
+  friend bool operator==(int lhs, Fraction rhs);
+  friend bool operator==(Fraction lhs, Fraction rhs);
+  friend bool operator==(Fraction lhs, int rhs);
+
+  friend bool operator!=(int lhs, Fraction rhs);
+  friend bool operator!=(Fraction lhs, Fraction rhs);
+  friend bool operator!=(Fraction lhs, int rhs);
+
+ private:
+  int num_, den_;
+};
+
+Fraction::Fraction():  num_(0), den_(1) {}
+Fraction::Fraction(int num, int den) {
+    if (den == 0) {
+        throw std::logic_error("");
+    }
+    num_ = num / gcd(num, den);
+    den_ = den / gcd(num, den);
+}
+
+Fraction::Fraction(int num) : num_(num), den_(1) {};
+Fraction::Fraction(const Fraction& other) {
+    num_ = other.num();
+    den_ = other.den();
+}
+
+
+Fraction& Fraction::operator=(Fraction& other) {
+    num_ = other.num();
+    den_ = other.den();
+    return *this;
+}
+
+Fraction operator+(Fraction lhs, Fraction rhs) {
+    int tmp_lcd = lcd(lhs.den_, rhs.den_);
+    lhs.num_ = lhs.num_ * (tmp_lcd / lhs.den_);
+    rhs.num_ = rhs.num_ * (tmp_lcd / rhs.den_);
+    lhs.num_ += rhs.num_;
+    lhs.den_ = tmp_lcd;
+
+    return Fraction(lhs.num_, lhs.den_);   
+}
+
+Fraction operator+(int lhs, Fraction rhs) {
+    Fraction tmp(lhs, 1);
+    return tmp + rhs;
+}
+
+Fraction operator+(Fraction lhs, int rhs) {
+    return rhs + lhs;
+}
+
+Fraction operator-(Fraction lhs, Fraction rhs) {
+    rhs.num_ = -rhs.num_;
+    return lhs + rhs;
+}
+
+Fraction operator-(int lhs, Fraction rhs) {
+    Fraction tmp(lhs, 1);
+    return tmp - rhs;
+}
+
+Fraction operator-(Fraction lhs, int rhs) {
+    return -rhs + lhs;
+}
+
+Fraction operator*(Fraction lhs, Fraction rhs) {
+    lhs.num_ *= rhs.num_;
+    lhs.den_ *= rhs.den_;
+    return lhs + 0;
+}
+
+Fraction operator*(int lhs, Fraction rhs) {
+    rhs.num_ *= lhs;
+    return rhs + 0;
+}
+
+Fraction operator*(Fraction lhs, int rhs) {
+    return rhs * lhs;
+}
+
+Fraction operator/(Fraction lhs, Fraction rhs) {
+    if (rhs.num_ == 0) 
+        throw std::logic_error("");
+    lhs.num_ *= rhs.den_;
+    lhs.den_ *= rhs.num_;
+    return lhs + 0;
+}
+
+Fraction operator/(int lhs, Fraction rhs) {
+    if (rhs.num_ == 0)
+        throw std::logic_error("");
+    int tmp = rhs.num_;
+    rhs.num_ = rhs.den_ * lhs;
+    rhs.den_ = tmp;
+    return rhs + 0;
+}
+
+Fraction operator/(Fraction lhs, int rhs) {
+    if (rhs == 0)
+        throw std::logic_error("");
+    lhs.den_ *= rhs;
+    return lhs + 0;
+}
+
+
+Fraction& Fraction::operator+=(Fraction other) {
+  Fraction tmp = *this + other;
+  *this = tmp;
+  return *this;
+}
+
+Fraction& Fraction::operator+=(int other) {
+    Fraction tmp = *this + other;
+    *this = tmp;
+    return *this;
+}
+
+
+Fraction& Fraction::operator-=(Fraction other) {
+    Fraction tmp = *this - other;
+    *this = tmp;
+    return *this;
+}
+
+Fraction& Fraction::operator-=(int other) {
+    Fraction tmp = *this - other;
+    *this = tmp;
+    return *this;
+}
+
+
+Fraction& Fraction::operator*=(Fraction other) {
+    Fraction tmp = *this * other;
+    *this = tmp;
+    return *this;
+}
+
+Fraction& Fraction::operator*=(int other) {
+    Fraction tmp = *this * other;
+    *this = tmp;
+    return *this;
+}
+
+
+Fraction& Fraction::operator/=(Fraction other) {
+    Fraction tmp = *this / other;
+    *this = tmp;
+    return *this;
+}
+
+Fraction& Fraction::operator/=(int other) {
+    Fraction tmp = *this / other;
+    *this = tmp;
+    return *this;
+}
+
+
+bool operator<(Fraction lhs, int rhs) {
+    return (lhs - rhs).num_ < 0;
+}
+
+bool operator<(Fraction lhs, Fraction rhs) {
+    return (lhs - rhs).num_ < 0;
+}
+
+bool operator<(int lhs, Fraction rhs) {
+    return (lhs - rhs).num_ < 0;
+}
+
+
+bool operator>(Fraction lhs, int rhs) {
+    return rhs < lhs;
+}
+
+bool operator>(Fraction lhs, Fraction rhs) {
+    return rhs < lhs;
+}
+
+bool operator>(int lhs, Fraction rhs) {
+    return rhs < lhs;
+}
+
+
+
+bool operator<=(Fraction lhs, int rhs) {
+    return !(lhs > rhs);
+}
+
+bool operator<=(Fraction lhs, Fraction rhs) {
+    return !(lhs > rhs);
+}
+
+bool operator<=(int lhs, Fraction rhs) {
+    return !(lhs > rhs);
+}
+
+
+
+bool operator>=(Fraction lhs, int rhs) {
+    return !(lhs < rhs);
+}
+
+bool operator>=(Fraction lhs, Fraction rhs) {
+    return !(lhs < rhs);
+}
+
+bool operator>=(int lhs, Fraction rhs) {
+    return !(lhs < rhs);
+}
+
+
+
+bool operator==(Fraction lhs, int rhs) {
+    return (lhs >= rhs) && (rhs >= lhs);
+}
+
+bool operator==(Fraction lhs, Fraction rhs) {
+    return (lhs >= rhs) && (rhs >= lhs);
+}
+
+bool operator==(int lhs, Fraction rhs) {
+    return (lhs >= rhs) && (rhs >= lhs);
+}
+
+
+
+bool operator!=(Fraction lhs, int rhs) {
+    return !(lhs == rhs);
+}
+
+bool operator!=(Fraction lhs, Fraction rhs) { return !(lhs == rhs); }
+
+bool operator!=(int lhs, Fraction rhs) { return !(lhs == rhs); }
+
+std::ostream& operator<<(std::ostream& out, const Fraction& fract) {
+  return out << fract.num() << "/" << fract.den();
+}
---
+++ class Vector/class Vector.cpp
@@ -0,0 +1,132 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+
+#include <iterator>
+#include <initializer_list>
+#include <stdexcept>
+#include <algorithm>
+
+
+class IntVector {
+public:
+    IntVector() : size_(0), capacity_(0), data_(nullptr) {}
+
+    // explicit конструктор с размером 
+    explicit IntVector(size_t size) :
+        size_(size), capacity_(size_), data_(new int[capacity_]) {
+        for (int i = 0; i < size_; ++i) {
+            data_[i] = 0;
+        }
+    }
+
+    // конструктор со списком инициализации 
+    explicit IntVector(std::initializer_list<int> list) :
+        size_(list.size()), capacity_(size_), data_(new int[capacity_]) {
+        const int* ptr = list.begin();
+
+        for (int i = 0; i < size_; ++i) {
+            data_[i] = *ptr;
+            ++ptr;
+        }
+    }
+
+    // конструктор копирования 
+    IntVector(const IntVector& other) {
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+        data_ = new int[capacity_];
+        std::copy(other.data_, other.data_ + other.size_, data_);
+    }
+
+    /* конструктор перемещения */
+    IntVector(IntVector&& other) :
+        size_(std::exchange(other.size_, 0)),
+        capacity_(std::exchange(other.capacity_, 0)),
+        data_(std::exchange(other.data_, nullptr))
+    { }
+
+    // деструктор 
+    ~IntVector() {
+        size_ = 0;
+        capacity_ = 0;
+        delete[] data_;
+    }
+
+    // оператор присваивания copy&swap 
+    IntVector& operator=(IntVector other) {
+        this->Swap(other);
+        return *this;
+    }
+
+    // const-метод Size 
+    size_t Size() const {
+        return size_;
+    }
+
+    // const-метод Capacity 
+    size_t  Capacity() const {
+        return capacity_;
+    }
+
+    // void-метод PushBack(int) 
+    void PushBack(int value) {
+        if (size_ == capacity_) {
+            capacity_ = 2 * size_;
+            if (capacity_ == 0) {
+                capacity_ = 1;
+            }
+
+            int* tmp = new int[capacity_];
+            std::copy(data_, data_ + size_, tmp);
+            delete[] data_;
+            data_ = tmp;
+        }
+        data_[size_++] = value;
+    }
+
+    // void-метод PopBack() 
+    void PopBack() {
+        --size_;
+    }
+
+    // void-метод Clear() 
+    void Clear() {
+        size_ = 0;
+    }
+
+    // void-метод Reserve() 
+    void Reserve(int capacity) {
+        if (capacity_ >= capacity) return;
+        int* tmp = new int[capacity];
+        std::copy(data_, data_ + size_, tmp);
+        delete[] data_;
+        data_ = tmp;
+
+        capacity_ = capacity;
+    }
+
+    void Swap(IntVector& rhs) {
+        std::swap(size_, rhs.size_);
+        std::swap(capacity_, rhs.capacity_);
+        std::swap(data_, rhs.data_);
+    }
+
+    // для доступа типа запись
+    int& operator[](size_t ind) {
+        return data_[ind];
+    }
+
+    // для доступа типа чтение
+    int operator[](size_t ind) const {
+        return data_[ind];
+    }
+
+private:
+    size_t size_;
+    size_t capacity_;
+    int* data_;
+};
+
+
